a1b2693cede751fd23134ccbc84f37864cc10283
Simon Larsen <slarse@kth.se>
2018-02-03 00:43:55 +0100

    Add tests for annotate

--- tests/test_decorator.py
+++ tests/test_decorator.py
@@ -8,10 +8,12 @@
 Author: Simon Larsén
 """
 import unittest
+import io
 from inspect import signature
 from unittest.mock import MagicMock, Mock, patch
 from .context import clanimtk
 from clanimtk.decorator import animate, ANNOTATED
+from clanimtk import annotate
 
 def animate_test_variables():
     """Return the variables for the animate tests."""
@@ -26,7 +28,7 @@ def animate_test_variables():
     return mock_function, return_value, docstring, mock_animation, step, animate_
 
 
-class DecoratorTest(unittest.TestCase):
+class AnimateTest(unittest.TestCase):
 
     def test_animate_does_not_modify_signature(self):
         def func(a, b, c):
@@ -43,9 +45,9 @@ class DecoratorTest(unittest.TestCase):
                           func=non_callable)
 
     @patch('clanimtk.decorator.get_supervisor', side_effect=lambda func: func)
-    def test_animate_with_constructor_kwargs(self, mock_get_supervisor):
+    def test_animate_with_decorator_kwargs(self, mock_get_supervisor):
         """This test emulates using kwargs in the decorator, so
-        that the constructor is actually called on the kwargs, and
+        that the decorator is actually called on the kwargs, and
         not on the function that animate decorates.
         """
         mock_function, return_value, docstring, mock_animation, step, animate_ = (
@@ -58,7 +60,7 @@ class DecoratorTest(unittest.TestCase):
         self.assertEqual(return_value, result)
 
     @patch('clanimtk.decorator.get_supervisor', side_effect=lambda func: func)
-    def test_animate_with_constructor_kwargs_and_function_args_and_kwargs(
+    def test_animate_with_decorator_kwargs_and_function_args_and_kwargs(
             self, mock_get_supervisor):
         args = ('herro', 2, lambda x: 2*x)
         kwargs = {'herro': 2, 'python': 42}
@@ -73,8 +75,8 @@ class DecoratorTest(unittest.TestCase):
         self.assertEqual(return_value, result)
 
     @patch('clanimtk.decorator.get_supervisor', side_effect=lambda func: func)
-    def test_animate_without_constructor_kwargs(self, mock_get_supervisor):
-        """Emulates decorating a function without calling the constructor explicitly."""
+    def test_animate_without_decorator_kwargs(self, mock_get_supervisor):
+        """Emulates decorating a function without calling the decorator explicitly."""
         mock_function, return_value, docstring, _, _, _ = (
             animate_test_variables())
         wrapped_function = animate(mock_function)
@@ -85,7 +87,7 @@ class DecoratorTest(unittest.TestCase):
         self.assertEqual(return_value, result)
 
     @patch('clanimtk.decorator.get_supervisor', side_effect=lambda func: func)
-    def test_animate_without_constructor_kwargs_with_function_args_and_kwargs(
+    def test_animate_without_decorator_kwargs_with_function_args_and_kwargs(
             self, mock_get_supervisor):
         args = ('herro', 2, lambda x: 2*x)
         kwargs = {'herro': 2, 'python': 42}
@@ -97,3 +99,71 @@ class DecoratorTest(unittest.TestCase):
         mock_function.assert_called_once()
         self.assertEqual(docstring, wrapped_function.__doc__)
         self.assertEqual(return_value, result)
+
+class AnnotateTest(unittest.TestCase):
+
+    def setUp(self):
+        self.doc = "This is just a stupid test function"
+        def func(abra, ka, dabra):
+            pass
+        func.__doc__ = self.doc
+        self.func = func
+
+    def test_annotate_raises_when_start_and_end_msgs_are_none(self):
+        self.assertRaises(
+            ValueError,
+            annotate)
+
+    def test_annotate_raises_when_start_is_not_none_nor_string(self):
+        self.assertRaises(
+            ValueError,
+            annotate,
+            start_msg=2)
+
+    def test_annotate_raises_when_end_msg_is_not_none_nor_string(self):
+        self.assertRaises(
+            ValueError,
+            annotate,
+            end_msg=2)
+
+    def test_annotate_does_not_modify_signature(self):
+        expected_params = signature(self.func).parameters.keys()
+        animated_func = annotate(start_msg='bogus')(self.func)
+        actual_params = signature(self.func).parameters.keys()
+        self.assertEqual(expected_params, actual_params)
+
+    def test_annotate_does_not_modify_doc(self):
+        expected_doc = self.doc
+        actual_doc = annotate(start_msg='herro')(self.func).__doc__
+        self.assertEqual(expected_doc, actual_doc)
+
+    def test_annotate_prints_only_start_msg_and_newline_when_end_msg_is_none(self):
+        with patch('sys.stdout', new=io.StringIO()) as mock_stdout:
+            msg = 'This is the start'
+            start_only = annotate(start_msg=msg)(self.func)
+            start_only(1,2,3) # 3 arbitrary arguments
+            self.assertEqual(msg + '\n', mock_stdout.getvalue())
+
+    def test_annotate_ommits_newline_after_start_msg_if_no_start_nl(self):
+        with patch('sys.stdout', new=io.StringIO()) as mock_stdout:
+            msg = 'This is the start'
+            start_only = annotate(start_msg=msg, start_no_nl=True)(self.func)
+            start_only(1,2,3) # 3 arbitrary arguments
+            self.assertEqual(msg, mock_stdout.getvalue())
+
+    def test_annotate_prints_only_end_msg_plus_newline_when_start_msg_is_none(self):
+        with patch('sys.stdout', new=io.StringIO()) as mock_stdout:
+            msg = 'This is the end'
+            end_only = annotate(end_msg=msg)(self.func)
+            end_only(1,2,3) # 3 arbitrary arguments
+            self.assertEqual(msg + '\n', mock_stdout.getvalue())
+
+    def test_annotate_prints_start_and_end_msgs_in_correct_order(self):
+        with patch('sys.stdout', new=io.StringIO()) as mock_stdout:
+            start_msg = 'This is the start'
+            end_msg = 'This is the end'
+            expected_print = start_msg + "\n" + end_msg + "\n"
+            annotated = annotate(start_msg=start_msg, end_msg=end_msg)(self.func)
+            annotated(1,2,3)
+            self.assertEqual(expected_print, mock_stdout.getvalue())
+
