e0cde629420c11f73d076835b6d76de9447b5937
Simon Larsén <slarse@kth.se>
2018-07-15 18:14:41 +0200

    Update __init__

--- clanimtk/__init__.py
+++ clanimtk/__init__.py
@@ -1,3 +1,5 @@
 from clanimtk.decorator import animate
 from clanimtk.decorator import Annotate as annotate
 from clanimtk.animation import animation
+
+__all__ = "animate animation annotate".split()
