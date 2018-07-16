f4eb4c8264300b265ef15cd2b9ec4822ca4ed764
Simon Larsén <slarse@kth.se>
2018-07-16 13:47:43 +0200

    Minor improvements to test_animate_cli

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -28,19 +28,26 @@ def test_erase(mock_write):
 
 
 def test_animate_cli(mock_flush, mock_write):
+    """Tests that animate cli writes to stdout and advances the
+    AnimationGenerator. 
+    
+    Because of the sleep, the test is probabilistic and may fail unexpectedly.
+    Any ideas for how to test this more reliably are super welcome.
+    """
     event = Event()
     animation_mock = MagicMock()
     char = '*'
     animation_mock.__next__ = MagicMock(return_value=char)
     animation_mock.get_erase_frame = MagicMock(return_value='')
-    step = .1
-    with patch('time.sleep'):
+    step = .01
+    with patch('time.sleep', autospec=True) as sleep_mock:
         thread = threading.Thread(
             target=cli.animate_cli, args=(animation_mock, step, event))
         thread.start()
     time.sleep(.5)
     event.set()  # terminate
 
+    sleep_mock.assert_called()
     mock_flush.assert_called()
     animation_mock.__next__.assert_called()
     mock_write.assert_any_call(char)
